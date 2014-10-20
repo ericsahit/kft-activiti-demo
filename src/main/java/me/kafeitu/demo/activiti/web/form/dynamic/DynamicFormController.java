1b8a929e8912e35743b664f3ec7fa86201024485
eric wong <win19999@gmail.com>
2014-10-20 17:18:06 +0800

    modify sample data for order workflow

--- src/main/java/me/kafeitu/demo/activiti/web/form/dynamic/DynamicFormController.java
+++ src/main/java/me/kafeitu/demo/activiti/web/form/dynamic/DynamicFormController.java
@@ -265,6 +265,10 @@ public class DynamicFormController {
                                  HttpServletRequest request) {
         ModelAndView mav = new ModelAndView("/form/dynamic/dynamic-form-task-list");
         User user = UserUtil.getUserFromSession(request.getSession());
+        // 用户未登录不能操作，实际应用使用权限框架实现，例如Spring Security、Shiro等
+        if (user == null || StringUtils.isBlank(user.getId())) {
+        	return new ModelAndView("redirect:/login?timeout=true"); 
+        }
 
         List<Task> tasks = new ArrayList<Task>();
 
